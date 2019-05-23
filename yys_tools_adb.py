50060d26c3a3a54271ef4ec0b02e08333f13431c
bevisy <binbin36520@gmail.com>
2019-05-24 05:01:47 +0800

    update

--- yys_tools_adb.py
+++ yys_tools_adb.py
@@ -54,7 +54,6 @@ class Tools(object):
                 center_x = int(center[0])
                 center_y = int(center[1])
                 self.rand_click(center_x, center_y)
-                self.log("click ({0}, {1})".format(center_x, center_y))
         except KeyboardInterrupt:
             self.log("identification click exited!\n")
 
@@ -64,27 +63,59 @@ class Tools(object):
         image_src = ac.imread(src)
         image_obj = ac.imread(obj)
         position = ac.find_template(image_src, image_obj)
-        if position['result'] is None:
-            return None
+        if position is None:
+            return False
         else:
-            return tuple([int(position['result'][0]), int(position['result'][1])])
+            # return tuple([int(position['result'][0]), int(position['result'][1])])
+            return True
 
     # Android Simulator 截图并取回本地
     @staticmethod
     def screenshot_adb(img):
-        # cmd = "adb exec-out screencap -p > sc.png"
-        cmd1 = "adb shell screencap -p sdcard/{0}".format(img)
-        cmd2 = "adb pull sdcard/{0}".format(img)
-        os.system(cmd1)
-        time.sleep(0.1)
-        os.system(cmd2)
+        cmd = "adb exec-out screencap -p > screen.png"
+        os.system(cmd)
+        # cmd1 = "adb shell screencap -p sdcard/{0}".format(img)
+        # cmd2 = "adb pull sdcard/{0}".format(img)
+        # os.system(cmd1)
+        # time.sleep(0.1)
+        # os.system(cmd2)
 
     # 屏幕截图
     @staticmethod
     def screenshot(img):
         pyautogui.screenshot(img)
 
+    def yaoqi(self):
+        try:
+            while True:
+                time.sleep(1)
+                self.screenshot_adb("screen.png")
+                if self.is_exists("screen.png", "images/zudui.png"):
+                    self.ident_click("screen.png", "images/zudui.png")
+                    self.log("zudui")
+                    time.sleep(0.1)
+                elif self.is_exists("screen.png", "images/zidongpipei.png"):
+                    if self.is_exists("screen.png", "images/pipeizhong.png"):
+                        time.sleep(5)
+                        self.log("pipeizhong")
+                    else:
+                        self.ident_click("screen.png", "images/zidongpipei.png")
+                        time.sleep(0.1)
+                        self.log("zidongpipei")
+                elif self.is_exists("screen.png", "images/zhunbei.png"):
+                    self.ident_click("screen.png", "images/zhunbei.png")
+                    time.sleep(0.1)
+                    self.log("zhunbei")
+                elif self.is_exists("screen.png", "images/jiesuan.png"):
+                    self.ident_click("screen.png", "images/jiesuan.png")
+                    time.sleep(0.1)
+                    self.log("jiesuan")
+                else:
+                    time.sleep(2)
+        except KeyboardInterrupt:
+            self.log("Exited!")
 
 if __name__ == "__main__":
     op = Tools()
     # op.loop_click(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]))
+    op.yaoqi()
