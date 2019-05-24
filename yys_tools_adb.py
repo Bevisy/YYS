00da49697cb54268de0c5ed470462ff26ce70549
bevisy <binbin36520@gmail.com>
2019-05-24 21:57:28 +0800

    add conv

--- yys_tools_adb.py
+++ yys_tools_adb.py
@@ -7,8 +7,8 @@ import pyautogui
 
 class Tools(object):
     def __init__(self):
-        self.SCREEN_X = 1440
-        self.SCREEN_Y = 728
+        self.SCREEN_X = 1024
+        self.SCREEN_Y = 640
         os.system("adb kill-server && adb server && adb devices")
 
     @staticmethod
